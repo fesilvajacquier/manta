46358c5107e027daf06952bdb51c0b890fbc8a2b
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2019-07-23 17:08:29 -0300

    unfinished seeds

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,6 @@
 class Report < ApplicationRecord
   belongs_to :user
   belongs_to :ngo
+  validates :category, :content, presence: true
+  validates :content, length: { minimun: 200 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# user
+puts 'creating users'
+fer = User.create!(email: 'fer@manta.com', password: '123456')
+orlando = User.create!(email: 'orlando@manta.com', password: '123456')
+pin = User.create!(email: 'pin@manta.com', password: '123456')
+yuyu = User.create!(email: 'yuyu@manta.com', password: '123456')
+
+# ngo
+puts 'creating ngos'
+
+# ngo_member
+puts 'creating ngo_members'
+
+# report
+puts 'creating reports'
+
+# publication
+puts 'creating publications'
+
+# offer
+puts 'creating offers'
+
+# message
+puts 'creating messages'
+
+# pictures
+puts 'creating pictures'
