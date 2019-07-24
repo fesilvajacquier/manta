5e8b9c22c8a1cbcbb8d43e10f6dc096a287f72bc
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2019-07-23 22:53:36 -0300

    Implement pundit. Add app policy & ngo policy

--- app/controllers/ngos_controller.rb
+++ app/controllers/ngos_controller.rb
@@ -1,5 +1,14 @@
 class NgosController < ApplicationController
+  before_action :set_ngo, only: %i[show]
+  skip_before_action :authenticate_user!, only: %i[show]
+
   def show
+  end
+
+  private
+
+  def set_ngo
     @ngo = Ngo.find(params[:id])
+    authorize @ngo
   end
 end
