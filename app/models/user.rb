1bfed4f5d9c2ca7e101fd0d2ff41ce1ab63082ff
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-24 14:52:27 -0300

    aouth configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
+
   def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -12,6 +13,7 @@ class User < ApplicationRecord
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
       user.refresh_token = auth.credentials.refresh_token
+      user.password = SecureRandom.urlsafe_base64
     end
   end
 end
