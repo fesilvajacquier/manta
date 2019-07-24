020a8e35eba9067270cefe1c2d57c3f4ac76b060
orpere <orlando.capoeiraraiz@gmail.com>
2019-07-24 12:11:35 -0300

    oauth

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'puma'
 gem 'rails', '5.2.3'
 gem 'redis'
 gem 'omniauth-google-oauth2'
+gem 'google-api-client'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
+require 'google/apis/people_v1'
+require 'google/api_client/client_secrets.rb'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  People = Google::Apis::PeopleV1
+
+  def contacts
+    secrets = Google::APIClient::ClientSecrets.new(
+      "web" => {
+        "access_token" => current_user.token,
+        "refresh_token" => current_user.refresh_token,
+        "client_id" => Rails.application.secrets[:google_client_id],
+        "client_secret" => Rails.application.secrets[:google_secret]
+      }
+    )
+    service = People::PeopleServiceService.new
+    service.authorization = secrets.to_authorization
+    response = service.list_person_connections(
+      'people/me',
+      person_fields: ['names', 'emailAddresses', 'phoneNumbers']
+    )
+    render json: response
+  end
 end
