1dd6f4f2d7569f2826155497d0a01f7cf59881b6
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2019-07-24 11:44:54 -0300

    add new and create methods to ngos controller

--- app/controllers/ngos_controller.rb
+++ app/controllers/ngos_controller.rb
@@ -5,7 +5,27 @@ class NgosController < ApplicationController
   def show
   end
 
+  def new
+    @ngo = NGO.new
+    authorize @ngo
+  end
+
+  def create
+    @ngo = Ngo.new(ngo_params)
+    @ngo.user = current_user
+    authorize @ngo
+    if @ngo.save
+      redirect_to ngo_path(@ngo)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def ngo_params
+    params.require(:ngo).permit(:name, :address, :email, :website, :description, :bank_account, :user_id, :created_at, :updated_at)
+  end
 
   def set_ngo
     @ngo = Ngo.find(params[:id])
