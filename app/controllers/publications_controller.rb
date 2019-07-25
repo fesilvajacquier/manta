257fcccb37e79426f218bd4482d6af2d69e7ecfd
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2019-07-25 12:31:48 -0300

    Model, Controller & Policy ready

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,4 +1,7 @@
 class PublicationsController < ApplicationController
+  before_action :set_publication, only: %i[show]
+  skip_before_action :authenticate_user!, only: %i[show new create]
+
   def index
     @publications = policy_scope(Publication).geocoded # returns flats with coordinates
 
@@ -8,5 +11,15 @@ class PublicationsController < ApplicationController
         lng: publication.longitude
       }
     end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_publication
+    @publication = Publication.find(params[:id])
+    authorize @publication
   end
 end
