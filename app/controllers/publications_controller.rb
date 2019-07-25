6f98160bfc3e37395e3dcf9e0e638c1428c81a30
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-07-25 15:31:14 -0300

    updated navbar and controllers

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,25 +1,25 @@
 class PublicationsController < ApplicationController
   skip_before_action :authenticate_user!
 
- def index
-  if params[:search].present?
-    @publications = policy_scope(Publication).search_publications(params[:search])
+  def index
+    if params[:search].present?
+      @publications = policy_scope(Publication).search_publications(params[:search])
 
-  @markers = @publications.map do |publication|
-    {
-      lat: publication.latitude,
-      lng: publication.longitude
-    }
+      @markers = @publications.map do |publication|
+        {
+          lat: publication.latitude,
+          lng: publication.longitude
+        }
+      end
+    else
+      @publications = policy_scope(Publication).geocoded # returns flats with coordinates
 
-  else
-    @publications = policy_scope(Publication).geocoded # returns flats with coordinates
-
-    @markers = @publications.map do |publication|
-      {
-        lat: publication.latitude,
-        lng: publication.longitude
-      }
+      @markers = @publications.map do |publication|
+          {
+            lat: publication.latitude,
+            lng: publication.longitude
+          }
+      end
     end
-   end
- end
+  end
 end
