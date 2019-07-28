ded0f194c4898e0c045e2adb64afc83b17e1c868
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-07-28 16:26:58 -0300

    Add Category model and migrate the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_214010) do
+ActiveRecord::Schema.define(version: 2019_07_28_192515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id"
