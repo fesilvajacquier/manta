ad8a4ea7c9ad7ead20991d4f6ac55219f0d61713
Plengpai Ratnajarn <pinplengpai@gmail.com>
2019-07-29 13:57:38 -0300

    add js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,22 @@
 import "bootstrap";
-import { reportSubmit } from "reports/report.js";
-import { search } from "./search";
-
-reportSubmit()
-search();
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
+import { reportSubmit } from "reports/report.js";
+reportSubmit()
+import { search } from "./search";
+search();
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/init_autocomplete.js';
-
 initMapbox();
-
-import '../components/message'
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
 initAutocomplete();
+import '../components/message'
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
+
+
+
+
+
+
+
